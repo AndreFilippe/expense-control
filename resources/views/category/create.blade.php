3dfb6986805d30cc85270ea59e277065d15a3545
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-02-11 12:27:19 -0300

    feat: created views category

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+</head>
+<body>
+    <form action="{{route('categories.store')}}" method="post">
+        @csrf
+        <label for="name">Name:</label>
+        <input type="text" name="name" required>
+        <br>
+        <label for="description">Description:</label>
+        <textarea name="description" rows="5"></textarea>
+        <br>
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
---
+++ resources/views/category/edit.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title}}</title>
+</head>
+<body>
+    <form action="{{route('categories.update',['category'=> $category->id])}}" method="post">
+        @csrf
+        @method('PUT')
+        <label for="name">Name:</label>
+        <input type="text" name="name" value="{{$category->name}}" required>
+        <br>
+        <label for="description">Description:</label>
+        <textarea name="description" rows="5">{{$category->description}}</textarea>
+        <br>
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
